34520e483e48e1edd976d3ae734121d9ba389b50
xomila <mnacheva05@gmail.com>
2025-10-31 10:46:17 +0100

    Fix duplicated code after merge

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const correctCode = "5683"; // your secret code
     let attempts = 0;
 
@@ -23,30 +22,3 @@ const correctCode = "5683"; // your secret code
           message.textContent = "Access Denied.";
         }
       }
-=======
-const correctCode = "5683"; // your secret code
-    let attempts = 0;
-
-    function checkCode() {
-      const userCode = document.getElementById("codeInput").value;
-      const message = document.getElementById("message");
-      const secretLink = document.getElementById("secretLink");
-
-      if (userCode === correctCode) {
-        message.style.color = "#44ff44";
-        message.textContent = "Access Granted.";
-        setTimeout(() => {
-          window.location.href = "room.html"; // next page
-        }, 1000);
-      } else {
-        attempts++;
-        message.style.color = "#ff4444";
-        message.textContent = `Incorrect Code (${attempts}/2)`;
-
-        if (attempts >= 2) {
-          secretLink.style.display = "block"; // show the hidden link
-          message.textContent = "Access Denied.";
-        }
-      }
->>>>>>> 7193360 (Initial commit)
-    }
