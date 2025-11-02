890f600e8cfbe01ae64798e1ac1466f193f46272
xomila <mnacheva05@gmail.com>
2025-11-03 00:06:08 +0100

    fixing java for index connection

--- little game/script.js
+++ little game/script.js
@@ -1,5 +1,26 @@
-function openApp(name) {
-    alert("Opening: " + name);
+function openApp(app) {
+    switch(app) {
+        case "My Files":
+            window.location.href = "../index.html";  
+            break;
+        case "Windows":
+            alert("Opening Windows Settings...");
+            break;
+        case "Internet Explorer":
+            window.open("https://google.com", "_blank"); 
+            break;
+        case "Games":
+            window.location.href = "games.html";  
+            break;
+        case "Command Prompt":
+            window.location.href = "../index.html";  
+            break;
+        case "Recycle Bin":
+            alert("Opening Recycle Bin...");
+            break;
+        default:
+            alert("Opening: " + app);
+    }
 }
 
 function updateClock() {
@@ -7,7 +28,6 @@ function updateClock() {
     const timeString = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
     document.getElementById("clock").textContent = timeString;
 }
-
 
 setInterval(updateClock, 1000);
 updateClock();
